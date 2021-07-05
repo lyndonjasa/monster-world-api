0b0bea10621c041014fae045863e1df769ea9494
MAGENIC\LyndonJ <lyndonj@magenic.com>
2021-07-05 16:47:51 +0800

    moved connection strings to env variables

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,11 @@ import config from './shared/config';
 import express from 'express';
 import routes from './routes/all-routes'
 
+import dotenv from 'dotenv'
+dotenv.config();
+
+console.log(process.env);
+
 const app = express();
 app.use(cors());
 app.use(express.json());
